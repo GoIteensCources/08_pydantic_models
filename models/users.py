e35a2c20ee98f4ed0e0cef168d1ffc46a722a9a5
santar4 <mdykovv@gmail.com>
2024-10-22 20:28:05 +0300

    models

--- models/users.py
+++ models/users.py
@@ -1,6 +1,7 @@
 import datetime
 
-from sqlalchemy import func
+import nullable
+from sqlalchemy import func, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from datetime import datetime
 
@@ -17,7 +18,7 @@ class User(Base):
     create_date: Mapped[datetime.datetime] = mapped_column(server_default=func.now())
     age: Mapped[int]
     role: Mapped[str] = mapped_column()
-
+    userdetails = relationship('UserDetails', back_populates='users')
 
 class UserDetails(Base):
     __tablename__ = "userdetails"
@@ -29,5 +30,9 @@ class UserDetails(Base):
     date_birth: Mapped[str] = mapped_column()
 
     country: Mapped[str] = mapped_column()
+
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
+
+    user = relationship('User', back_populates='userdetails')
 
 
